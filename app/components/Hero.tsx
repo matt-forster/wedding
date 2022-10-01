329f6c2839485d8b9702cb6b01ece5e4fc6ef2ea
Matt Forster <hey@mattforster.ca>
2022-09-30 19:53:21 -0600

    feat: improve hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,34 +2,36 @@ export default function First() {
   return (
     <div
       id="hero"
-      className="bg-cover bg-center-bottom bg-no-repeat h-screen w-screen grid place-items-center justify-center"
+      className="bg-cover bg-center-bottom bg-no-repeat h-screen w-screen grid place-items-center"
       style={{
         backgroundImage: "url('/images/space.jpg')",
       }}
     >
-      <div className="text-center">
-        <div>You are invited to the third-attempt reception of</div>
-        <h1 className="text-3xl lg:text-6xl text-[#eceff4]">
-          Matthew & Dominique
-        </h1>
-        <div className="text-3xl lg:text-6xl">Forster</div>
-      </div>
+      <div className="h-screen w-screen grid place-items-center justify-center bg-gradient-to-b from-black/90 via-transparent">
+        <div className="text-center">
+          <div>You are invited to the third-attempt reception of</div>
+          <h1 className="text-3xl lg:text-6xl text-[#eceff4]">
+            Matthew & Dominique
+          </h1>
+          <div className="text-3xl lg:text-6xl">Forster</div>
+        </div>
 
-      <div className="text-center bg-grey lg:text-3xl mb-24">
-        Azure Ridge Estate,
-        <br />
-        Foothills, AB
-        <br />
-        Saturday, September 2nd, 2023
-      </div>
+        <div className="grid w-full h-1/3 text-center place-items-center bg-grey lg:text-3xl mb-24 rounded-lg">
+          Azure Ridge Estate,
+          <br />
+          Foothills, AB
+          <br />
+          Saturday, September 2nd, 2023
+        </div>
 
-      <div className="border-t border-slate-300 flex flex-row lg:gap-80 text-xl pt-8">
-        <a className="pr-10 ml-8 font-bold hover:text-[#81a1c1]" href="#info">
-          About
-        </a>
-        <a className="pl-10 mr-8 font-bold hover:text-[#81a1c1]" href="#rsvp">
-          RSVP
-        </a>
+        <div className="border-t border-slate-300 flex flex-row lg:gap-80 text-xl pt-8">
+          <a className="pr-10 ml-8 font-bold hover:text-[#81a1c1]" href="#info">
+            About
+          </a>
+          <a className="pl-10 mr-8 font-bold hover:text-[#81a1c1]" href="#rsvp">
+            RSVP
+          </a>
+        </div>
       </div>
     </div>
   );
